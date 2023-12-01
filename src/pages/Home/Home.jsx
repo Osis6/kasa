a7204dd6a55677b9f28980552d81c6c765fbe926
BOUNOU <djeey.osis@gmail.com>
2023-12-01 18:46:04 +0100

    . .

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,29 +1,28 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Banner from "../../components/Banner/Banner";
 import Card from "../../components/Cards/Card";
 import { Link } from "react-router-dom";
-import axios from "axios";
-
+import logementsData from "../../datas/logements.json";
 
 export default function Home() {
-	const [data, setData] = useState([]);
-  
-	useEffect(() => {
-	  axios.get("/logements.json").then((res) => setData(res.data));
-	}, []);
-  
-	return (
-	  <>
-		<Banner text="Chez vous, partout et ailleurs" customClass="home-banner" />
-		<div className="cards-container">
-		  {data.map((appart, id) => (
-			<div className="card_logement" key={id}>
-			  <Link className="link_card_logement" to={`/logement/${appart.id}`}>
-				<Card cover={appart.cover} title={appart.title} />
-			  </Link>
-			</div>
-		  ))}
-		</div>
-	  </>
-	);
-  }
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setData(logementsData);
+  }, []);
+
+  return (
+    <>
+      <Banner text="Chez vous, partout et ailleurs" customClass="home-banner" />
+      <div className="cards-container">
+        {data.map((appart, id) => (
+          <div className="card_logement" key={id}>
+            <Link className="link_card_logement" to={`/logement/${appart.id}`}>
+              <Card cover={appart.cover} title={appart.title} />
+            </Link>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
