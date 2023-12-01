8d9ad8d5d3cf272895c0a304d22033a5b4afb5c2
BOUNOU <djeey.osis@gmail.com>
2023-12-01 20:12:34 +0100

    test

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import Router from "./router.jsx";
 import "./style/main.scss";
+import HomeLayout from "./pages/Home/HomeLayout";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <Router />
+    <HomeLayout />
   </BrowserRouter>
 );
--- src/router.jsx
+++ src/router.jsx
@@ -1,41 +1,19 @@
 import React from "react";
-import { Routes, Route, Outlet } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home.jsx";
 import About from "./pages/About/About.jsx";
 import FicheLogement from "./pages/FicheLogement/FicheLogement.jsx";
 import Error from "./pages/Error/Error.jsx";
-import Header from "./components/Header/Header.jsx";
-import Footer from "./components/Footer/Footer.jsx"; 
+import HomeLayout from "./pages/Home/HomeLayout.jsx";
 
-const HomeLayout = ({ children }) => {
-  return <div>{children}</div>;
-};
 
 const Router = () => {
   return (
     <Routes>
-      <Route
-        path="/"
-        element={
-          <div className="App">
-            <Header />
-            <main>
-              <Outlet />
-            </main>
-            <Footer />
-          </div>
-        }
-      >
-        <Route
-          index
-          element={
-            <HomeLayout>
-              <Home />
-            </HomeLayout>
-          }
-        />
-        <Route path="/about" element={<About />} />
-        <Route path="/logement/:id" element={<FicheLogement />} />
+      <Route path="/" element={HomeLayout} >
+        <Route index  element={<Home />}/>
+        <Route path="about" element={<About />}/>
+        <Route path="logement/:id" element={<FicheLogement />} />
         <Route path="*" element={<Error />} />
       </Route>
     </Routes>
